f976bea7b9f361eaefee6b7f3e277ff7bcb1c975
Vishnu Kumar <vk2579789@gmail.com>
2026-05-23 23:03:15 +0530

    Fixed authRoute imports

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,18 +1,17 @@
-const express =require('express');
-const router =express.Router();
-const verifyToken =require('../middleware/verifyToken');
-const asyncHandler =require('../utils/asyncHandler');
-const validateRequest =require('../middleware/validationMiddleware');
-const { registerValidation, loginValidation }=require('../validations/authValidation');
-const { updateProfileValidation }=require('../validations/profileValidation');
-const { register, login }=require('../controllers/authController');
-const { logout, getMe }=require('../controllers/authProfileController');
-const { updateProfile }=require('../controllers/profileUpdateController');
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
+const asyncHandler = require("../utils/asyncHandler");
+const validateRequest = require("../middleware/validationMiddleware");
+const {registerValidation, loginValidation} = require("../validations/authValidation");
+const { updateProfileValidation } = require("../validations/profileValidation");
+const { register, login, logout } = require("../controllers/authController");
+const {getProfile, updateProfile} = require("../controllers/profileController");
 
-router.post('/register', registerValidation, validateRequest, asyncHandler(register));
-router.post('/login', loginValidation, validateRequest, asyncHandler(login));
-router.post('/logout', verifyToken, asyncHandler(logout));
-router.get('/me', verifyToken, asyncHandler(getProfile));
-router.put('/update', verifyToken, profileValidation, validateRequest, asyncHandler(updateProfile));
+router.post("/register", registerValidation, validateRequest, asyncHandler(register),);
+router.post("/login", loginValidation, validateRequest, asyncHandler(login));
+router.post("/logout", verifyToken, asyncHandler(logout));
+router.get("/me", verifyToken, asyncHandler(getProfile));
+router.put("/update", verifyToken, updateProfileValidation, validateRequest, asyncHandler(updateProfile));
 
 module.exports = router;
