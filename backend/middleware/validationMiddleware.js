39bc1f68702d117d8311132647ee30a8fd8cdd59
Vishnu Kumar <vk2579789@gmail.com>
2026-05-24 13:07:10 +0530

    Added validation debug logs

--- backend/middleware/validationMiddleware.js
+++ backend/middleware/validationMiddleware.js
@@ -1,28 +1,17 @@
-const { validationResult } =
-require('express-validator');
+const { validationResult } = require("express-validator");
 
-const validateRequest=
-(req,res,next)=>{
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors=
-    validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("PROFILE VALIDATION:", errors.array());
 
-    if(!errors.isEmpty()){
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
 
-        return res.status(400).json({
-
-            success:false,
-
-            errors:
-            errors.array()
-
-        });
-
-    }
-
-    next();
-
+  next();
 };
 
-module.exports=
-validateRequest;
+module.exports = validateRequest;
