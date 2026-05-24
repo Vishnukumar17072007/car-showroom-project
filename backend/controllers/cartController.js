c7cbd1266a5249675446f76b7228117ba1db8d5a
Vishnu Kumar <vk2579789@gmail.com>
2026-05-24 11:56:11 +0530

    Fixed cart controller schema mismatch

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,49 +1,43 @@
 const Cart = require("../models/CartListSchema");
 
 const getCart = async (req, res) => {
-  const cart = await Cart.findOne({ user: req.user.userId }).populate('items.carId');
+  const cart = await Cart.findOne({userId: req.user.userId}).populate("items.carId");
 
-  res.status(200).json(cart || { cars: [] });
+  res.status(200).json( cart || { items: [] });
 };
 
 const addToCart = async (req, res) => {
-  let cart = await Cart.findOne({ user: req.user.userId });
+  let cart = await Cart.findOne({userId: req.user.userId});
 
   if (!cart) {
-    cart = new Cart({ user: req.user.userId, cars: [] });
+    cart = new Cart({ userId: req.user.userId, items: [] });
   }
 
-  cart.cars.push({ carId:req.body.carId, quantity: req.body.quantity || 1 });
+  const exists = cart.items.some((item) => item.carId.toString() === req.body.carId);
+
+  if (!exists) {
+    cart.items.push({ carId: req.body.carId });
+  }
 
   await cart.save();
 
-  res.status(200).json({
-    message: "Added to cart",
-
-    cart,
-  });
+  res.status(200).json({ message: "Added to cart", cart });
 };
 
 const removeFromCart = async (req, res) => {
-  const cart = await Cart.findOne({
-    user: req.user.userId,
-  });
+  const cart = await Cart.findOne({userId: req.user.userId});
 
   if (!cart) {
     const error = new Error("Cart not found");
-
     error.status = 404;
-
     throw error;
   }
 
-  cart.cars = cart.cars.filter((item) => item.carId.toString() === req.params.id);
+  cart.items = cart.items.filter((item) => item.carId.toString() !== req.params.id);
 
   await cart.save();
 
-  res.status(200).json({
-    message: "Removed from cart",
-  });
+  res.status(200).json({ message: "Removed from cart" });
 };
 
-module.exports = { getCart, addToCart, removeFromCart };
+module.exports = { getCart, addToCart, removeFromCart};
