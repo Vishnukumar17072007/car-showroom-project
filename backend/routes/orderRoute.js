aae0741e2d57b76e1a0d48d63854abc9adea09a2
Vishnu Kumar <vk2579789@gmail.com>
2026-05-24 12:28:39 +0530

    Restored admin order status route

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,15 +2,49 @@ const express = require("express");
 const router = express.Router();
 
 const verifyToken = require("../middleware/verifyToken");
+
 const verifyRole = require("../middleware/verifyRole");
+
 const asyncHandler = require("../utils/asyncHandler");
-const { createOrder, getUserOrders, getAllOrders} = require("../controllers/orderController");
-const { updateOrderStatus, deleteOrder } = require("../controllers/orderAdminController");
+
+const {
+  createOrder,
+  getUserOrders,
+  getAllOrders,
+} = require("../controllers/orderController");
+
+const {
+  updateOrderStatus,
+  deleteOrder,
+} = require("../controllers/orderAdminController");
+
+// USER
 
 router.post("/", verifyToken, asyncHandler(createOrder));
+
 router.get("/", verifyToken, asyncHandler(getUserOrders));
-router.get("/admin/all", verifyToken, verifyRole("admin"), asyncHandler(getAllOrders));
-router.patch("/cancel/:id", verifyToken, asyncHandler(updateOrderStatus));
-router.delete("/delete/:id", verifyToken, asyncHandler(deleteOrder));
+
+// ADMIN
+
+router.get(
+  "/admin/all",
+  verifyToken,
+  verifyRole("admin"),
+  asyncHandler(getAllOrders),
+);
+
+router.put(
+  "/admin/:id/status",
+  verifyToken,
+  verifyRole("admin"),
+  asyncHandler(updateOrderStatus),
+);
+
+router.delete(
+  "/admin/:id",
+  verifyToken,
+  verifyRole("admin"),
+  asyncHandler(deleteOrder),
+);
 
 module.exports = router;
